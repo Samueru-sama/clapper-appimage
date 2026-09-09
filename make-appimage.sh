e1258132aa274c7e742ac61a498bda6171cc5bd1
Samueru-sama <xdglawyer@outlook.com>
2026-09-08 22:54:48 -0400

    build clapper manually

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,8 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q clapper | awk '{print $2; exit}')
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
@@ -16,18 +15,11 @@ export GTK_CLASS_FIX=1
 export STRACE_BINARY=clapper
 export STRACE_FLAGS=https://test-videos.co.uk/vids/bigbuckbunny/mp4/h265/1080/Big_Buck_Bunny_1080_10s_1MB.mp4
 
-sys_clapper_dir=$(echo /usr/lib/clapper-*)
-if [ -d "$sys_clapper_dir" ]; then
-	export PATH_MAPPING="
-		$sys_clapper_dir:\${SHARUN_DIR}/lib/${sys_clapper_dir##*/}
-	"
-else
-	>&2 echo "ERROR: Cannot find the clapper lib dir"
-	exit 1
-fi
-
 # Trace and deploy all files and directories needed for the application (including binaries, libraries and others)
 quick-sharun /usr/bin/clapper
+
+# Make Clapper load its importer modules from inside the AppImage
+echo 'CLAPPER_SINK_IMPORTER_PATH=${SHARUN_DIR}/lib/clapper-0.0/gst/plugin/importers' >> ./AppDir/.env
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
